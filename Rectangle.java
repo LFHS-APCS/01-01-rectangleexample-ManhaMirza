5916d79765ab59feed52d8773a03ec5a6c054305
ManhaMirza <69738443+ManhaMirza@users.noreply.github.com>
2020-10-13 12:23:02 -0500

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -53,7 +53,7 @@ public class Rectangle
      */
     public int getY()
     {
-
+       return this.y;
     }
 
     /**
@@ -72,6 +72,7 @@ public class Rectangle
      */
     public void setY(int theY)
     {
+      this.y = theY;
 
     }
 
@@ -82,6 +83,7 @@ public class Rectangle
     public void move(int rightX, int upY)
     {
         x = x + rightX;
+        y = y + upY;
 
     }
 
